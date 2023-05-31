c60898d063737533370aeee12b46f226df603533
Divine Edwin <edwindivine5@gmail.com>
2023-05-31 16:26:21 +0100

    Created Pages
    I created home, about and errorpage components and linked them with routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from "react"
 import { BrowserRouter as Router, Routes, Route, } from "react-router-dom"
 import Navbar from "./components/layouts/Navbar"
 import Footer from "./components/layouts/Footer"
+import Home from "./Pages/Home"
+import About from "./Pages/About"
+import ErrorPage from "./Pages/ErrorPage"
 
 const App = () => {
 
@@ -11,7 +14,14 @@ const App = () => {
         <div className="flex flex-col justify-between h-screen">
           <Navbar />
 
-          <main className="container mx-auto px-3 pb-12">Content</main>
+          <main className="container mx-auto px-3 pb-12">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/errorpage" element={<ErrorPage />} />
+              <Route path="/*" element={<ErrorPage />} /> 
+            </Routes>
+          </main>
 
           <Footer />
         </div>
