3d1f2d94e61b0c42d7bac879ba118464f3e608a2
Divine Edwin <edwindivine5@gmail.com>
2023-05-31 15:53:12 +0100

    Initial commit
    Created the layout folder and navbar with footer components, also imported react-router-dom

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import { BrowserRouter as Router, Routes, Route, } from "react-router-dom"
+import Navbar from "./components/layouts/Navbar"
+import Footer from "./components/layouts/Footer"
+
+const App = () => {
+
+  return (
+    <>
+      <Router>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar />
+
+          <main className="container mx-auto px-3 pb-12">Content</main>
+
+          <Footer />
+        </div>
+      </Router>
+    </>
+  )
+}
+
+export default App
