469e091951c089823fe21c0427931784aa8bad2f
Divine Edwin <edwindivine5@gmail.com>
2023-06-01 12:00:28 +0100

    Github Context component
    Moved all states and function component from the user results to the github context and cleaned the user results component.

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,30 @@ import Footer from "./components/layouts/Footer"
 import Home from "./Pages/Home"
 import About from "./Pages/About"
 import ErrorPage from "./Pages/ErrorPage"
+import { GithubProvider } from "./contexts/GithubContext"
 
 const App = () => {
 
   return (
     <>
-      <Router>
-        <div className="flex flex-col justify-between h-screen">
-          <Navbar />
+      <GithubProvider>
+        <Router>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
 
-          <main className="container mx-auto px-3 pb-12">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/errorpage" element={<ErrorPage />} />
-              <Route path="/*" element={<ErrorPage />} /> 
-            </Routes>
-          </main>
+            <main className="container mx-auto px-3 pb-12">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/errorpage" element={<ErrorPage />} />
+                <Route path="/*" element={<ErrorPage />} />
+              </Routes>
+            </main>
 
-          <Footer />
-        </div>
-      </Router>
+            <Footer />
+          </div>
+        </Router>
+      </GithubProvider>
     </>
   )
 }
--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,41 +1,27 @@
-import { useEffect, useState } from "react"
+import { useEffect, useContext } from "react"
+import Spinner from "../layouts/Spinner"
+import UserItem from "./UserItem"
+import GithubContext from "../../contexts/GithubContext"
 
 const UserResults = () => {
-    // const githubToken = import.meta.env.VITE_GITHUB_TOKEN;
-    // const githubUrl = import.meta.env.VITE_GITHUB_URL;
-    const [users, setUsers] = useState([])
-    const [loading, setLoading] = useState(true)
+const {users, loading, fetchUsers} = useContext(GithubContext)
 
     useEffect(() => {
         fetchUsers()
     }, [])
-
-    const fetchUsers = async () => {
-        const response = await fetch(`${import.meta.env.VITE_GITHUB_URL}/users`, {
-            headers: {
-                Authorization: `token ${import.meta.env.VITE_GITHUB_TOKEN}`,
-            },
-        })
-
-        const data = await response.json()
-
-        // console.log(data)
-        setUsers(data)
-        setLoading(false)
-    }
 
     if (!loading) {
         return (
             <>
                 <div className="grid grid-cols-1 gap-0 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
                     {users.map((user) => (
-                        <h3>{user.login}</h3>
+                        <UserItem key={user.id} user={user} />
                     ))}
                 </div>
             </>
         )
     } else {
-        return <h3>Loading...</h3>
+        return <Spinner /> 
     }
 }
 
