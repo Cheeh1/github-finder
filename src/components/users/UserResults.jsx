56c73c4280942b36b6fe7ed9d1d8366418c9f016
Divine Edwin <edwindivine5@gmail.com>
2023-06-03 19:44:56 +0100

    fixed a bug
    Solved issue of empty array and making it not display users

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,28 +1,47 @@
-import { useEffect, useContext } from "react"
+import { useContext } from "react"
 import Spinner from "../layouts/Spinner"
 import UserItem from "./UserItem"
 import GithubContext from "../../contexts/GithubContext"
 
 const UserResults = () => {
-const {users, loading, fetchUsers} = useContext(GithubContext)
 
-    useEffect(() => {
-        fetchUsers()
-    }, [])
+    const { users, loading } = useContext(GithubContext)
 
     if (!loading) {
-        return (
-            <>
-                <div className="grid grid-cols-1 gap-0 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-                    {users.map((user) => (
-                        <UserItem key={user.id} user={user} />
-                    ))}
-                </div>
-            </>
-        )
+        
+        if (Array.isArray(users.items) && users.items.length > 0) {
+            return (
+                <>
+                    <div className="grid grid-cols-1 gap-0 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+                        {users.items.map((user) => (
+                            <UserItem key={user.id} user={user} />
+                        ))}
+                    </div>
+                </>
+            );
+        } else {
+            // Handle the case when users.items is an empty array
+            return <div>No users found.</div>;
+        }
     } else {
-        return <Spinner /> 
+        return <Spinner />;
     }
+
+
+    // if (!loading) {
+    //     console.log(users)
+    //     return (
+    //         <>
+    //             <div className="grid grid-cols-1 gap-0 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+    //                 {users.map((user) => (
+    //                     <UserItem key={user.id} user={user} />
+    //                 ))}
+    //             </div>
+    //         </>
+    //     )
+    // } else {
+    //     return <Spinner /> 
+    // }
 }
 
 export default UserResults
