1d97dd3480237858d501cd12218d9060ce97caa9
Divine Edwin <edwindivine5@gmail.com>
2023-06-03 21:45:10 +0100

    Fixed Error.
    Corrected the code i changed earlier while fixing a bug

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -8,40 +8,19 @@ const UserResults = () => {
     const { users, loading } = useContext(GithubContext)
 
     if (!loading) {
-        
-        if (Array.isArray(users.items) && users.items.length > 0) {
-            return (
-                <>
-                    <div className="grid grid-cols-1 gap-0 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-                        {users.items.map((user) => (
-                            <UserItem key={user.id} user={user} />
-                        ))}
-                    </div>
-                </>
-            );
-        } else {
-            // Handle the case when users.items is an empty array
-            return <div>No users found.</div>;
-        }
+        console.log(users)
+        return (
+            <>
+                <div className="grid grid-cols-1 gap-0 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+                    {users.map((user) => (
+                        <UserItem key={user.id} user={user} />
+                    ))}
+                </div>
+            </>
+        )
     } else {
-        return <Spinner />;
+        return <Spinner /> 
     }
-
-
-    // if (!loading) {
-    //     console.log(users)
-    //     return (
-    //         <>
-    //             <div className="grid grid-cols-1 gap-0 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-    //                 {users.map((user) => (
-    //                     <UserItem key={user.id} user={user} />
-    //                 ))}
-    //             </div>
-    //         </>
-    //     )
-    // } else {
-    //     return <Spinner /> 
-    // }
 }
 
 export default UserResults
